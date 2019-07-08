a457ac96260ec6878eae1d0e43f080c5b99740ab
hash <hassaanhashmy@gmail.com>
2019-07-08 20:11:10 +0500

    updated

--- VulnerbilityPatch.sh
+++ VulnerbilityPatch.sh
@@ -6,3 +6,7 @@ wget https://raw.githubusercontent.com/hassanhashmy/scripts/master/RepoInstall.s
 chmod +x RepoInstall.sh
 ./RepoInstall.sh
 yum update -y;
+yum install kernel-ml.x86_64
+awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg
+grub2-set-default 0
+package-cleanup --oldkernels --count=1
